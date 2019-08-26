035441ee2249159ba5aa702e1ac84f8c1e884a25
ManhBanh <52659954+ManhBanh@users.noreply.github.com>
2019-08-26 16:51:45 +0700

    Trần Quang Mạnh

--- Project/WatchVideo/app/src/main/java/vn/edu/devpro/watchvideo/ItemCategory/WatchItemCategory.java
+++ Project/WatchVideo/app/src/main/java/vn/edu/devpro/watchvideo/ItemCategory/WatchItemCategory.java
@@ -1,11 +1,15 @@
 package vn.edu.devpro.watchvideo.ItemCategory;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.VideoView;
+
+import java.util.ArrayList;
 
 import vn.edu.devpro.watchvideo.R;
 
@@ -13,19 +17,30 @@ public class WatchItemCategory extends AppCompatActivity {
     VideoView vvWatchItemCategory;
     MediaController mediaController;
 
+    Toolbar tbWatchItemCategory;
+    TextView tvItemCategoryTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watch_item_category);
+        this.setTitle(null);
+
+        tbWatchItemCategory = findViewById(R.id.tbWatchItemCategory);
+        tvItemCategoryTitle = findViewById(R.id.tvItemCategoryTitle);
 
         vvWatchItemCategory = findViewById(R.id.vvWatchItemCategory);
 
-        ItemCategory itemCategory = (ItemCategory)getIntent().getSerializableExtra("videoitemcategory");
+        ItemCategory itemCategory = (ItemCategory) getIntent().getSerializableExtra("videoitemcategory");
+        ArrayList<ItemCategory> itemCategoryArrayList = (ArrayList<ItemCategory>) getIntent().getSerializableExtra("itemCategoryArrayList");
 
+        setSupportActionBar(tbWatchItemCategory);
+        tvItemCategoryTitle.setText(String.valueOf(itemCategoryArrayList.size()));
         if(mediaController == null){
             mediaController = new MediaController(this);
             vvWatchItemCategory.setMediaController(mediaController);
             mediaController.setAnchorView(vvWatchItemCategory);
+            
         }
 
         try{
